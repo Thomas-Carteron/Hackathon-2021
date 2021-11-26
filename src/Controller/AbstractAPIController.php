df214b97303dca1c98cb04aeba4ea5f63692da29
Thomas <thomas.carter98@gmail.com>
2021-11-26 02:17:11 +0100

    fix insert query for ngrock request

--- src/Controller/AbstractAPIController.php
+++ src/Controller/AbstractAPIController.php
@@ -6,27 +6,20 @@ use App\Model\ApiManager;
 
 class AbstractAPIController
 {
-    public function __construct()
-    {
-        header('Content-Type: application/json');
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: POST,GET,PUT,DELETE, OPTIONS");
-        header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept");
-    }
+ //   public function __construct()
+   // {
+    //    header('Content-Type: application/json');
+     //   header("Access-Control-Allow-Origin: *");
+      //  header("Access-Control-Allow-Methods: POST,GET,PUT,DELETE, OPTIONS");
+       // header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept");
+    //}
 
-    public function index(string $name, int $time)
+    public function index()
     {
+        $name = $_GET['name'];
+        $time = $_GET['time'];
         $apiManager = new ApiManager();
         $apiManager->insertNgrokInBdd($name, $time);
-        header('Location:/ngrok.html.twig');
-    }
-
-    public function insert(string $name, int $time)
-    {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $apiManager = new ApiManager();
-            $apiManager->insertNgrokInBdd2($name, $time);
-            header('Location:/ngrok.html.twig');
-        }
+        header('Location:/ngrok');
     }
 }
--- src/Model/ApiManager.php
+++ src/Model/ApiManager.php
@@ -4,24 +4,15 @@ namespace App\Model;
 
 class ApiManager extends AbstractManager
 {
-    public const TABLE = 'user';
+    public const TABLE = 'leader_board';
 
     public function insertNgrokInBdd(string $name, int $time)
     {
         $statement = $this->pdo->prepare("
-        INSERT INTO user (name, best_time)
+        INSERT INTO leader_board (name, best_time)
         VALUES (:name, :best_time)");
         $statement->bindValue(':name', $name, \PDO::PARAM_STR);
-        $statement->bindValue(':time', $time, \PDO::PARAM_INT);
-        $statement->execute();
-    }
-    public function insertNgrokInBdd2(string $name, int $time)
-    {
-        $statement = $this->pdo->prepare("
-        INSERT INTO user (name, best_time)
-        VALUES (:name, :best_time)");
-        $statement->bindValue(':name', $name, \PDO::PARAM_STR);
-        $statement->bindValue(':time', $time, \PDO::PARAM_INT);
+        $statement->bindValue(':best_time', $time, \PDO::PARAM_INT);
         $statement->execute();
     }
 }
